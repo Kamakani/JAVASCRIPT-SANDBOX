76a81a1fdc16dd8fc71b14bf3686807b8a1a3659
Derek <37349773+Kamakani@users.noreply.github.com>
2023-11-29 09:58:40 -0700

    control of variables and data types. (#3)

---
+++ 01-variables-data-types/04-primitive-data-types/script.js
@@ -0,0 +1,20 @@
+// DATA TYPES
+
+// String - Sequences of characters, Must be in quotes or backticks
+// Number - Integers as well as floating-point numbers
+// Boolean - Logical entity / true or false
+// Null - Intentional absence of any object value
+// Undefined - A Variable that has not yet been defiuned / assigned
+// Symbol, - Built-in object whose constructor returns a unique symbol
+// BigInt - Numbers that are greater than the "Number" type can handle
+
+// REFERENCE TYPES (Objects)
+
+// Reference types or "objects are a non-primitive value and when assigned to a variable, the variable is given a reference to that value.
+// Object literals, arrays and functions are all reference types
+
+// STATIC TYPING VS DYNAMIC TYPING
+
+// JavaScriupt is a dynamically-types language. This means, we do not explicitly define the types for our vartiables. Many other languages are statically-typed such as C, C++ and Java
+
+// TypeScript is a superset of JavaScript, which allows static -typing. This can make your code more verbose and less prone to errors. TypeScript may be something you want to look into later on.
