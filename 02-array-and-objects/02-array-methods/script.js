935d2af6a6ffa578c6ed655759ea87ffafff3938
Derek White <derekbwhite80@gmail.com>
2024-02-09 16:39:45 -0700

    created array methods index/script

---
+++ 02-array-and-objects/02-array-methods/script.js
@@ -0,0 +1,23 @@
+let x;
+
+const arr = [23,35,47,53,12];
+
+// arr.fill(1,0);
+
+// add to and remove from end of array
+// arr.push(100);
+// arr.pop();
+// add to and remove from beginning of array
+// arr.unshift(99);
+// arr.shift();
+// reverses the array
+// arr.reverse();
+
+// x = arr.includes(100);
+// x = arr.indexOf(23);
+x = arr.slice(2,4);
+// x = arr.splice(1,3);
+// x = arr.splice(3, 1);
+// x = arr.splice(1,4).reverse().toString().charAt();
+
+console.log(arr, x);
