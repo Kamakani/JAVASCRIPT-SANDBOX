28f54d18a1d75c05081ab5f5d67de26f00f7a508
Derek White <derekbwhite80@gmail.com>
2024-02-17 13:34:06 -0700

    updated files in function basics

---
+++ 03-function-scope-execution/01-function-basics/script.js
@@ -0,0 +1,22 @@
+// Function - Name - Parenthesis - Curly Braces
+
+function sayHello() {
+  console.log('Hello World');
+}
+// Invoking or calling a function
+sayHello();
+// defining or declaring a function
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+add(5, 10);
+
+function subtract(num1, num2) {
+  return num1 - num2;
+  
+}
+
+const result = subtract(10, 2);
+
+console.log(result, subtract(20, 5));
